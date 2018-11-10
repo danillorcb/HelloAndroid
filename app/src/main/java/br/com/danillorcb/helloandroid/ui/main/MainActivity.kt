40f7a7b8ee98b72504fa9642d5daa9c317cedaa4
danillorcb <danilloramosbezerra@gmail.com>
2018-11-10 17:27:10 -0200

    Envio checkout concluído

--- app/src/main/java/br/com/danillorcb/helloandroid/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/danillorcb/helloandroid/ui/main/MainActivity.kt
@@ -1,20 +1,66 @@
 package br.com.danillorcb.helloandroid.ui.main
 
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.danillorcb.helloandroid.R
+import br.com.danillorcb.helloandroid.model.Pedido
+import br.com.danillorcb.helloandroid.ui.checkout.CheckoutActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val nome = intent.getStringExtra("nome")
-        val telefone = intent.getStringExtra("telefone")
+        mainViewModel = (ViewModelProviders
+                .of(this)
+                .get(MainViewModel::class.java)
+                )
 
-        tvNome.text = getString(R.string.saudacao, nome, telefone)
+        mainViewModel.nomeCliente = intent.getStringExtra("nome")
+        mainViewModel.telefoneCliente = intent.getStringExtra("telefone")
 
+        tvNome.text = getString(R.string.saudacao,
+                mainViewModel.nomeCliente,
+                mainViewModel.telefoneCliente)
+
+        cbAtum.isChecked = mainViewModel.atumSelecionado
+        cbBacon.isChecked = mainViewModel.baconSelecionado
+        cbCalabresa.isChecked = mainViewModel.calabresaSelecionada
+        cbMussarela.isChecked = mainViewModel.mussarelaSelecionada
+
+        cbAtum.setOnCheckedChangeListener { buttonView, isChecked ->
+            mainViewModel.atumSelecionado = isChecked
+        }
+
+        cbBacon.setOnCheckedChangeListener { buttonView, isChecked ->
+            mainViewModel.baconSelecionado = isChecked
+        }
+
+        cbCalabresa.setOnCheckedChangeListener { buttonView, isChecked ->
+            mainViewModel.calabresaSelecionada = isChecked
+        }
+
+        cbMussarela.setOnCheckedChangeListener { buttonView, isChecked ->
+            mainViewModel.mussarelaSelecionada = isChecked
+        }
+
+        btCalcular.setOnClickListener {
+            val intent = Intent(this, CheckoutActivity::class.java)
+            intent.putExtra("pedido", gerarPedido())
+            startActivity(intent)
+        }
+    }
+
+    private fun gerarPedido(): Pedido {
+        return Pedido(
+                mainViewModel.nomeCliente,
+                mainViewModel.telefoneCliente
+        )
     }
 }
--- app/src/main/java/br/com/danillorcb/helloandroid/ui/main/MainViewModel.kt
+++ app/src/main/java/br/com/danillorcb/helloandroid/ui/main/MainViewModel.kt
@@ -1,2 +1,15 @@
 package br.com.danillorcb.helloandroid.ui.main
 
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    var nomeCliente = ""
+    var telefoneCliente = ""
+    var atumSelecionado = false
+    var baconSelecionado = false
+    var calabresaSelecionada = false
+    var mussarelaSelecionada = false
+
+
+}
